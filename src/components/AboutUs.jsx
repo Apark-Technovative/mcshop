93592207643167f04f9013d6c7beb60bedbe6a5f
Ashim0 <dongolashim11@gmail.com>
2026-01-26 11:55:55 +0545

    fix: sizes and minor css

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function AboutUs({ showButton = true }) {
@@ -6,14 +6,14 @@ export default function AboutUs({ showButton = true }) {
 
   return (
     <section className="relative bg-white overflow-hidden min-h-[85vh]">
-      
+
       <span className="absolute top-10 left-10 w-10 h-10 border-2 border-green-500 rounded-full"></span>
       <span className="absolute top-1/2 right-1/2 w-6 h-6 border-2 border-green-500 rotate-45"></span>
       <span className="absolute top-12 right-20 w-6 h-6 border-2 border-indigo-500 rounded-full"></span>
       <span className="absolute bottom-16 right-24 w-8 h-8 border-2 border-indigo-500 rotate-45"></span>
 
       <div className="max-w-7xl mx-auto px-6 py-28 grid grid-cols-1 lg:grid-cols-2 gap-16 items-center h-full">
-        
+
         <div className="flex justify-center">
           <img
             src="/images/printer.png"
@@ -38,7 +38,7 @@ export default function AboutUs({ showButton = true }) {
             projects are handled with care and precision.
           </p>
 
-          
+
           {showButton && (
             <button
               onClick={() => navigate("/about")}
@@ -52,7 +52,7 @@ export default function AboutUs({ showButton = true }) {
 
       </div>
 
-      <div className="w-full h-[2px] bg-gray-300 mt-10"></div>
+      <div className="w-full h-0.5 bg-gray-300 mt-10"></div>
     </section>
   );
 }
